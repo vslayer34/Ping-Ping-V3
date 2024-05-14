2924c1b8e2e3443f7fb733827fb520af99e3f355
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-14 17:35:44 +0300

    Added a terget group cinemachine camera

--- Assets/Scripts/Camera/CameraSizeAdjuster.cs
+++ Assets/Scripts/Camera/CameraSizeAdjuster.cs
@@ -11,23 +11,32 @@ namespace PingPing.Scripts.CameraScripts
         private CinemachineVirtualCamera _virtualCamera;
 
         private float _cameraAspect;
+        private float _phonePixelWidth;
         private const float _mobilesSizeModifier = 2.3f;
         private const float _tabletsSizeModifierMultiplier = 6.4f;
+
+        private const float _standardCameraSize = 8.5f;
+        private const float _standardPixelWidth = 2048;
 
 
         private void Start()
         {
-            _cameraAspect = Camera.main.aspect;
-            Debug.Log(_cameraAspect);
 
-            if (_cameraAspect < 2)
-            {
-                _virtualCamera.m_Lens.OrthographicSize = _tabletsSizeModifierMultiplier * _cameraAspect;
-            }
-            else
-            {
-                _virtualCamera.m_Lens.OrthographicSize = _mobilesSizeModifier * _cameraAspect;
-            }
+            //  Size 8.5 is suitable for 2048 pixel width
+            _cameraAspect = Camera.main.aspect;
+            _phonePixelWidth = Camera.main.pixelWidth;
+            Debug.Log(Camera.main.scaledPixelWidth);
+
+            _virtualCamera.m_Lens.OrthographicSize = _phonePixelWidth * _standardCameraSize / _standardPixelWidth;
+
+            // if (_cameraAspect < 2)
+            // {
+            //     _virtualCamera.m_Lens.OrthographicSize = _tabletsSizeModifierMultiplier * _cameraAspect;
+            // }
+            // else
+            // {
+            //     _virtualCamera.m_Lens.OrthographicSize = _mobilesSizeModifier * _cameraAspect;
+            // }
         }
     }
 }

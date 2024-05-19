d8f376e5ed0a419c341415b3c245b1d57d0c496c
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2024-05-19 18:38:31 +0300

    Testing different layout values

--- Assets/Scripts/Platforms/PlatformController.cs
+++ Assets/Scripts/Platforms/PlatformController.cs
@@ -99,6 +99,7 @@ namespace PingPing.Scripts.Platforms
                 else
                 {
                     _movementDirection = Vector2.left + (_touchWorldPosition.y - transform.position.y > 0 ? Vector2.up : Vector2.down);
+                    // _movementDirection = (_touchWorldPosition.y - transform.position.y > 0 ? Vector2.up : Vector2.down);
                     _movementDirection = _movementDirection.normalized;
                 }
 
